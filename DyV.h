6b7f611bb98c9de66360beb4ba30414cce886ea2
Tomaspc809 <tomaspc809@gmail.com>
2023-10-29 19:26:33 +0100

    Implementada la función QuickSort.

--- DyV.h
+++ DyV.h
@@ -36,3 +36,32 @@ template <typename T> int BusquedaBinariaINV(T x, std::vector<T>& v, int ini, in
 
 	return BusquedaBinariaINV(x, v, medio + 1, fin);
 }
+
+template <typename T> int Particion(std::vector<T>& v, int ini, int fin){
+	T x = v[fin];
+	T aux;
+	int i = ini;
+
+	for (int j = ini; j < fin; j++){
+		if (v[j] <= x){
+			aux = v[i];
+			v[i] = v[j];
+			v[j] = aux;
+
+			i++;
+		}
+	}
+	aux = v[i];
+	v[i] = v[fin];
+	v[fin] = aux;
+	
+	return i;
+}
+
+template <typename T> void QuickSort(std::vector<T>& v, int ini, int fin){
+	if (ini < fin){
+		int pivote = Particion(v, ini, fin);
+		QuickSort(v, ini, pivote - 1);
+		QuickSort(v, pivote + 1, fin);
+	}
+}
